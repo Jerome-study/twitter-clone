3b2edd044e09ede5d86067c5ab5a16974ce2538f
jerome <jerome.studypurpose@gmail.com>
2024-06-15 15:16:02 +0800

    replace the useMediaquery importing to useResponsive hook

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,11 @@
 import { SideNavbar } from "./sideNav";
-import { Toolbar, useMediaQuery, useTheme } from "@mui/material";
+import { Toolbar } from "@mui/material";
 import { TopBar } from "./topBar";
 import { useState } from "react";
+import { useResponsive } from "../../hooks/useResponsive";
 
 export const Navbar = () => {
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+    const { isMobile } = useResponsive();
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     const toggleDrawer = () => {
