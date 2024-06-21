cf9aeedc1eaf0535d1432dae38f3ab095816fad4
jerome <jerome.studypurpose@gmail.com>
2024-06-21 18:00:26 +0800

    seperate states, custom hooks and handling fuction

--- src/components/form/TweetComposer.tsx
+++ src/components/form/TweetComposer.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
-import { useAuth } from '../../context/authProvider';
-import { tweetSchema } from '../../models/zod';
-import { useFireStore } from '../../config/firebase';
 import { Box, TextField, IconButton, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { ConfirmationModal } from '../Modal/Confirmation';
+import { useTweetComposer } from '../../hooks/useTweetComposer';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import SendIcon from '@mui/icons-material/Send';
 import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
-import { ConfirmationModal } from '../Modal/Confirmation';
-import { useFirebaseStorage } from '../../config/firebase';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -23,64 +19,7 @@ const VisuallyHiddenInput = styled('input')({
 });
 
 export const TweetComposerForm = ({ toggleDrawer }: { toggleDrawer?: Function }) => {
-    const { doc, setDoc, collection, db, serverTimestamp } = useFireStore();
-    const { storage, ref, uploadBytes, getDownloadURL } = useFirebaseStorage();
-    const { currentUser } = useAuth();
-    const [tweetContent, setTweetContent] = useState("");
-    const [tweetImages, setTweetImages] = useState([])
-    const [validateContent, setValidateContent] = useState("");
-    const [openModal, setOpenModal] = useState(false);
-
-    const handleOpenModal = (e: any) => {
-        e.preventDefault();
-        setOpenModal(prev => !prev)
-    }
-
-    const handleImageChange = (e : any) => {
-        if (e.target.files.length > 0) {
-          const selectedImages :any = Array.from(e.target.files);
-          setTweetImages(selectedImages);
-        }
-      };
-
-    const postTweet = async () => {
-        setValidateContent("");
-        const tweet = {
-            user_id: currentUser?.uid || "",
-            content: tweetContent,
-            createdAt: serverTimestamp(),
-            updatedAt: serverTimestamp(),
-        }
-        try {
-            const validationResult = tweetSchema.safeParse(tweet)
-            if (validationResult.error) return handleValidationErrors(validationResult.error);
-            const tweetId = doc(collection(db, 'tweets')).id;
-            const imageUrls = await Promise.all(
-                tweetImages.map(async (image, index) => {
-                    const imageRef = ref(storage, `tweets/${currentUser.uid}/${tweetId}/image${index + 1}`);
-                    await uploadBytes(imageRef, image);
-                    return getDownloadURL(imageRef);
-                })
-            );
-            await setDoc(doc(db, 'tweets', tweetId), { ...tweet, image: imageUrls });
-            if (toggleDrawer) toggleDrawer();
-        } catch (error: any) {
-            console.log(error)
-        } finally {
-            setTweetContent("")
-        }
-    }
-
-    const handleValidationErrors = (error: any) => {
-        const errors = error?.errors;
-
-        if (errors.length > 1) {
-            setValidateContent(errors[1].message)
-        } else {
-            setValidateContent(errors[0].message)
-        }
-        console.log(errors)
-    };
+    const { validateContent, openModal, tweetContent, handleOpenModal, postTweet, handleImageChange, setTweetContent } = useTweetComposer({toggleDrawer})
 
     return (
         <>
