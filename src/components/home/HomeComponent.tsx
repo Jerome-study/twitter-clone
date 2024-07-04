d75006ae7a756cf29465066be9fb5dd8d8c6d541
jerome <jerome.studypurpose@gmail.com>
2024-07-04 22:31:44 +0800

    add the useGetTweets

--- src/components/home/HomeComponent.tsx
+++ src/components/home/HomeComponent.tsx
@@ -1,10 +1,23 @@
+import { Tweets } from "../tweets/Tweets";
 import { TweetComposer } from "./TweetComposer";
+import { useAuth } from "../../context/authProvider";
+import { useGetTweets } from "../../hooks/useGetTweets";
 
 export const HomeComponent = () => {
+  const { currentUser } = useAuth();
+  const { currentUserTweets, loading } = useGetTweets(currentUser.uid);
+
+  if (loading) return <h1>Loading</h1>
 
   return (
     <>
       <TweetComposer />
+      { loading && <h1>Loading</h1>}
+      {currentUserTweets?.map((currentUserTweet: any) => {
+        return (
+          <Tweets currentUserTweet={currentUserTweet} key={currentUserTweet.id} />
+        )
+      })}
     </>
   );
 }
