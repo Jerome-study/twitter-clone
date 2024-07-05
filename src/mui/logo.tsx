795c3fb8ac33ab30f8096a3242930219717b0a71
jerome <jerome.studypurpose@gmail.com>
2024-07-05 17:46:09 +0800

    add reusable component logo

---
+++ src/mui/logo.tsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography';
+
+export const Logo = ({ inTopBar } : any) => {
+  return (
+    <Typography pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', color: inTopBar ? '#fff' : '#1DA1F2' }}>
+      SocialTweet
+    </Typography>
+  );
+};
+
