0de726168f26ebc7b8cd0862cc1fa1aef764e0d7
jerome <jerome.studypurpose@gmail.com>
2024-07-10 18:19:29 +0800

    modify design and fix the structure

--- src/components/requiredAuth/requiredAuth.tsx
+++ src/components/requiredAuth/requiredAuth.tsx
@@ -6,6 +6,26 @@ import { Navbar } from "../navbar/navbar";
 import { BottomNavbar } from "../navbar/bottomNav";
 import { useMediaQuery, useTheme } from "@mui/material";
 
+
+const MainContent = ({ children }: { children: ReactNode }) => (
+    <Box sx={{ mt: { lg: 0, xs: 2 }, bgcolor: { lg: "#ccc"} }} flexGrow={1}>
+        {children}
+    </Box>
+);
+
+const RightSidebar = () => (
+    <Box hidden sx={{
+        position: "relative",
+        display: { lg: "block" },
+        height: "100vh",
+        width: "15%"
+    }}>
+        <Box sx={{ borderLeft: '1px solid #ccc', height: "100vh"}} position="fixed">
+            Right Content
+        </Box>
+    </Box>
+);
+
 export const RequiredAuth = ({ children }: { children: ReactNode }) => {
     const { currentUser, loading } = useAuth();
     const theme = useTheme();
@@ -15,32 +35,10 @@ export const RequiredAuth = ({ children }: { children: ReactNode }) => {
 
     return (
         <>
-            <Box sx={{
-                display: {
-                    lg: "flex"
-                }
-            }}>
-                <Box>
-                    <Navbar />
-                </Box>
-
-                <Box flexGrow={1}>
-                    {children}
-                </Box>
-                
-                <Box hidden sx={{
-                    position: "relative",
-                    display: {
-                        lg: "block"
-                    },
-                    height: "100vh",
-                    width: "15%"
-                }}>
-                    <Box sx={{ borderLeft:.5, height: "100vh"}} position="fixed">
-                        Right Content
-                    </Box>
-                </Box>
-                
+            <Box sx={{ display: { lg: "flex" } }}>
+                <Navbar />
+                <MainContent>{children}</MainContent>
+                <RightSidebar />
                 <Toolbar />
             </Box>
             {isMobile && <BottomNavbar />}
