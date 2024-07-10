8ba436ed74529df6c1ceb6dd8a50511ce68e3775
jerome <jerome.studypurpose@gmail.com>
2024-07-10 18:17:42 +0800

    remove relative position

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,10 +1,9 @@
-import { Box } from "@mui/material"
 import { HomeComponent } from "../../components/home/HomeComponent"
 
 export const HomePage = () => {
     return (
-        <Box sx={{ position: "relative"}}>
+        <>
             <HomeComponent />
-        </Box>
+        </>
     )
 }
