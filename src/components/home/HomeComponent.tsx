7f77a6b9798a99802f3e8e31903f94d9bc7ed4e3
jerome <jerome.studypurpose@gmail.com>
2024-07-10 18:19:02 +0800

    add home name top of main content

--- src/components/home/HomeComponent.tsx
+++ src/components/home/HomeComponent.tsx
@@ -2,22 +2,37 @@ import { Tweets } from "../tweets/Tweets";
 import { TweetComposer } from "./TweetComposer";
 import { useAuth } from "../../context/authProvider";
 import { useGetTweets } from "../../hooks/useGetTweets";
+import { Box, Typography } from "@mui/material";
 
 export const HomeComponent = () => {
   const { currentUser } = useAuth();
   const { currentUserTweets, loading } = useGetTweets(currentUser.uid);
 
-  if (loading) return <h1>Loading</h1>
+  if (loading) return <Typography variant="h1" bgcolor={"white"} height={"100vh"}>Loading</Typography>
 
   return (
     <>
-      <TweetComposer />
-      { loading && <h1>Loading</h1>}
+      <Typography 
+        hidden 
+        sx={{ p:2, bgcolor: 'White', display: {
+          lg: 'block'
+        }}} 
+        component="h1" 
+        variant="h5" 
+        fontWeight={800}
+      >
+        Home
+      </Typography>
+      <Box sx={{ borderTop: '1px solid #ccc', borderBottom: '1px solid #ccc', bgcolor: 'White', mb: { lg: 2} }}>
+        <TweetComposer />
+      </Box>
+
       {currentUserTweets?.map((currentUserTweet: any) => {
         return (
           <Tweets currentUserTweet={currentUserTweet} key={currentUserTweet.id} />
         )
       })}
+
     </>
   );
 }
