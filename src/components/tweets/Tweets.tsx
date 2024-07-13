742d0d73ca62f23504d351c54f4c6237b5d44af0
jerome <jerome.studypurpose@gmail.com>
2024-07-13 22:19:36 +0800

    add loading skeleton for image and names and add action bar for each tweet

--- src/components/tweets/Tweets.tsx
+++ src/components/tweets/Tweets.tsx
@@ -1,28 +1,17 @@
-import { Card, Box, Avatar, Typography, CardMedia, CardContent, Grid } from '@mui/material';
-
-
-const TweetImages = ({ images }: any) => {
-    return (
-        <Box sx={{ display: "grid", gridTemplateColumns: images.length > 1 ? "repeat(2, 1fr)" : "", gap: 1}}>
-            {images.map((image: string, index: number) => {
-                return (
-                    <CardMedia
-                        key={image}
-                        component="img"
-                        sx={{ borderRadius: 3, boxShadow: 2, height: "100%", gridRow: (images.length === 3 && index === 0) ? "span 2" : ""}}
-                        
-                        image={image}
-                        alt="tweet image"
-                    />
-                )
-            })}
-        </Box>
-    )
-}
-
+import { Card, Box, Avatar, Typography, CardContent} from '@mui/material';
+import { useGetTweetsUserInfo } from '../../hooks/useGetUserInfo';
+import Skeleton from '@mui/material/Skeleton';
+import { TweetImages } from './TweetImages';
+import { useResponsive } from '../../hooks/useResponsive';
+import { ActionComponent } from './ActionComponent';
 
 export const Tweets = ({ currentUserTweet }: any) => {
     const avatarUrl = 'https://via.placeholder.com/150';
+    const { isMobile } = useResponsive();
+    const { userInfo, loading, error } = useGetTweetsUserInfo(currentUserTweet.user_id);
+
+    if (error) return <Typography>{error}</Typography>
+    const { first_name, last_name } = userInfo;
 
     return (
         <>
@@ -31,13 +20,14 @@ export const Tweets = ({ currentUserTweet }: any) => {
                     <Avatar alt="Remy Sharp" src={avatarUrl} />
                 </Box>
                 <Box sx={{ flex: 1 }}>
-                    <Typography>{currentUserTweet.user_id}</Typography>
-                    <CardContent sx={{ px: 0 }}>
-                        <Typography variant="body2" color="text.secondary" mb={1}>
+                    {loading ? <Skeleton /> : <Typography variant={isMobile ? "subtitle1" : "h6"} fontWeight={900} >{first_name + " " + last_name}</Typography>}
+                    <CardContent sx={{ px: 0, py: 1 }}>
+                        <Typography variant={isMobile ? "subtitle2" : "subtitle1"} fontWeight={600} mb={2}>
                             {currentUserTweet.content}
                         </Typography>
                         {currentUserTweet.image?.length > 0 && <TweetImages images={currentUserTweet.image} />}
                     </CardContent>
+                    <ActionComponent />
                 </Box>
             </Card>
         </>
