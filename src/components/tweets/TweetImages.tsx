dafefb52d010d70dc638edd8cdbac1c1c29a35a3
jerome <jerome.studypurpose@gmail.com>
2024-07-18 00:14:26 +0800

    Tweet Images layout added

--- src/components/tweets/TweetImages.tsx
+++ src/components/tweets/TweetImages.tsx
@@ -1,40 +1,54 @@
 import { Box, CardMedia, Skeleton } from "@mui/material";
 import { Fragment, useState } from "react";
 
-export const TweetImages = ({ images }: any) => {
-
+export const TweetImages = ({ images }: { images: string[] }) => {
     return (
-        <Box sx={{mb: 2, display: "grid", gridTemplateColumns: images.length > 1 ? "repeat(2, 1fr)" : "", gap: 1 }}>
-            {images.map((image: string, index: number) => {
+        <Box 
+            sx={{
+                mb: 2,
+                display: "grid",
+                gridTemplateColumns: images.length > 1 ? "repeat(2, 1fr)" : "1fr",
+                gap: 1
+            }}
+        >
+            {images.map((image, index) => {
                 const [isLoaded, setIsLoaded] = useState(false);
 
-                const handleImageLoad = () => {
-                    setIsLoaded(true);
-                };
+                const handleImageLoad = () => setIsLoaded(true);
+
+                const gridRow = images.length === 3 && index === 0 ? "span 2" : "";
+
+                const paddingTop = images.length === 3 && index === 0 ? '120%' : '57%';
 
                 return (
                     <Fragment key={index}>
+                        {!isLoaded && (
+                            <Skeleton 
+                                variant="rectangular" 
+                                sx={{
+                                    paddingTop,
+                                    borderRadius: 3,
+                                    boxShadow: 2,
+                                    gridRow
+                                }} 
+                            />
+                        )}
                         <CardMedia
                             component="img"
-                            sx={{ display: "none", borderRadius: 3, boxShadow: 2, height: "100%", gridRow: (images.length === 3 && index === 0) ? "span 2" : "" }}
+                            sx={{
+                                display: isLoaded ? "block" : "none",
+                                borderRadius: 3,
+                                boxShadow: 2,
+                                height: "100%",
+                                gridRow
+                            }}
                             onLoad={handleImageLoad}
                             image={image}
                             alt="tweet image"
                         />
-                        {!isLoaded &&
-                            <Skeleton variant='rectangular' sx={{ paddingTop: (images.length === 3 && index === 0) ? '100%' : '57%', borderRadius: 3, boxShadow: 2, gridRow: (images.length === 3 && index === 0) ? "span 2" : "" }} />
-                        }
-                        {isLoaded &&
-                            <CardMedia
-                                component="img"
-                                sx={{ borderRadius: 3, boxShadow: 2, height: "100%", gridRow: (images.length === 3 && index === 0) ? "span 2" : "" }}
-                                image={image}
-                                alt="tweet image"
-                            />
-                        }
                     </Fragment>
-                )
+                );
             })}
         </Box>
-    )
-}
+    );
+};
