99cc5c10eb7bd7da4e682ad17f5a30bb320aa6d5
jerome <jerome.studypurpose@gmail.com>
2024-07-17 23:33:48 +0800

    replace the drawer by the reusable bottom drawer

--- src/components/home/MobileTweetComposer.tsx
+++ src/components/home/MobileTweetComposer.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { Fab, Drawer, Box } from '@mui/material';
+import { Fab } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { TweetComposerForm } from '../form/TweetComposer';
+import { BottomDrawer } from '../mui/BottomDrawer';
 
 export const MobileTweetComposer = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -19,23 +20,9 @@ export const MobileTweetComposer = () => {
             >
                 <AddIcon />
             </Fab>
-            <Drawer
-                anchor="bottom"
-                open={isOpen}
-                onClose={() => setIsOpen(false)}
-                PaperProps={{
-                    sx: {
-                        width: '100%',
-                        height: '100%', // Set the height to fill the entire viewport
-                        position: 'absolute',
-                        bottom: 0,
-                    },
-                }}
-            >
-                <Box p={2}>
-                    <TweetComposerForm toggleDrawer={toggleDrawer}/>
-                </Box>
-            </Drawer>
+            <BottomDrawer isOpen={isOpen} setIsOpen={setIsOpen}>
+                <TweetComposerForm toggleDrawer={toggleDrawer} />
+            </BottomDrawer>
         </>
     );
 }
