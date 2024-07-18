58827af06befff0ee2b97666ec0381bce831bafb
jerome <jerome.studypurpose@gmail.com>
2024-07-18 13:52:10 +0800

    Wrap the children in Tweet Provider

--- src/components/requiredAuth/requiredAuth.tsx
+++ src/components/requiredAuth/requiredAuth.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { useAuth } from "../../context/authProvider";
 import { Navigate } from "react-router-dom";
 import { Box, Grid } from "@mui/material";
@@ -6,6 +6,7 @@ import { Navbar } from "../navbar/navbar";
 import { BottomNavbar } from "../navbar/bottomNav";
 import { NavigationLeftList } from "../navbar/List";
 import { useResponsive } from "../../hooks/useResponsive";
+import { TweetProvider } from "../../context/tweetProvider";
 
 const MainContent = ({ children }: { children: ReactNode }) => (
     <Box sx={{ mt: { lg: 0, xs: 2 }, width: "100%", bgcolor: { lg: "#ccc" } }} >
@@ -21,8 +22,8 @@ const RightSidebar = () => (
         height: '100vh',
         width: '100%',
         borderLeft: '1px solid #ccc',
-        backgroundColor: '#f9f9f9', // Example background color
-        zIndex: 1, // Ensure it's above content
+        backgroundColor: '#f9f9f9',
+        zIndex: 1,
     }}>
         Right Content
     </Box>
@@ -38,8 +39,8 @@ const NavbarAndLeftSidebar = () => {
             height: { lg: "100vh" },
             width: '100%',
             borderRight: '1px solid #ccc',
-            backgroundColor: '#f9f9f9', // Example background color
-            zIndex: 1, // Ensure it's above content
+            backgroundColor: '#f9f9f9',
+            zIndex: 1,
         }}>
             {isMobile ? <Navbar /> : <NavigationLeftList />}
         </Box>
@@ -49,26 +50,30 @@ const NavbarAndLeftSidebar = () => {
 export const RequiredAuth = ({ children }: { children: ReactNode }) => {
     const { currentUser, loading } = useAuth();
     const { isMobile } = useResponsive();
+    const [currentAction, setCurrentAction] = useState("Home");
+    const condition = currentAction === "Home" ? children : currentAction === "Explore" ? <h1>Search</h1> : <h1>Coming Soon</h1>
+    const handleBottomNavAction = (action: string) => {
+        setCurrentAction(action)
+    }
+
     if (loading) return <h1>loading</h1>
     if (!currentUser) return <Navigate to={'/login'} />
 
     return (
-        <>
+        <TweetProvider>
             <Grid container>
                 <Grid item lg={3} xs={12}>
                     <NavbarAndLeftSidebar />
                 </Grid>
                 <Grid item lg={6} xs={12}>
-                    <MainContent>{children}</MainContent>
+                    <MainContent>{condition}</MainContent>
                 </Grid>
                 <Grid item lg={3} xs={12}>
                     <RightSidebar />
                 </Grid>
             </Grid>
-
-            
-            {isMobile && <BottomNavbar />}
-        </>
+            {isMobile && <BottomNavbar handleBottomNavAction={handleBottomNavAction} />}
+        </TweetProvider>
     )
 
 }
