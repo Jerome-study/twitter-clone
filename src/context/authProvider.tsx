454d74a61d2402fd008503fc22ee33ce0e2d35a3
jerome <jerome.studypurpose@gmail.com>
2024-07-19 17:21:49 +0800

    add a function to find if  current user has username

--- src/context/authProvider.tsx
+++ src/context/authProvider.tsx
@@ -1,6 +1,6 @@
 import { ReactNode, createContext, useContext, useEffect, useState } from "react"
 import { useFirebaseAuth } from "../config/firebase";
-
+import { useFireStore } from "../config/firebase";
 
 const AuthContext = createContext<any>({});
 
@@ -12,11 +12,19 @@ export const AuthProvider = ({ children } : { children : ReactNode }) => {
     const { auth, onAuthStateChanged } = useFirebaseAuth()
     const [currentUser, setCurrentUser] = useState<any | null>(null);
     const [loading, setLoading] = useState(true);
+    const { db, doc, getDoc } = useFireStore();
+    const [isUsernameExist, setIsUsernameExist] = useState(false)
 
     useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
+        onAuthStateChanged(auth, async (user) => {
             if (user) {
-                setCurrentUser(user)
+                const docRef = doc(db, 'users', user.uid);
+                setCurrentUser(user);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    const { username } = docSnap.data();
+                    setIsUsernameExist(username? true : false)
+                }
             } else {
                 setCurrentUser(null);
             }
@@ -25,7 +33,7 @@ export const AuthProvider = ({ children } : { children : ReactNode }) => {
     }, []);
 
     return(
-        <AuthContext.Provider value={{ currentUser, loading }}>
+        <AuthContext.Provider value={{ currentUser, loading, isUsernameExist }}>
             {children}
         </AuthContext.Provider>
     )
