301a592ebfd67bb61c77724da599017f1ac47b8e
jerome <jerome.studypurpose@gmail.com>
2024-07-20 20:59:26 +0800

    change navigate into window location

--- src/hooks/useFormValidatioin.ts
+++ src/hooks/useFormValidatioin.ts
@@ -1,28 +1,25 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { formSchema, FormProps } from "../models/zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useNavigate } from "react-router-dom";
 import { useFireStore, useFirebaseAuth } from "../config/firebase";
 import { useState } from "react";
 
-export const useFormValidation = (formType : string) => {
-    const { db,setDoc, doc, getDoc } = useFireStore();
+export const useFormValidation = (formType: string) => {
+    const { db, setDoc, doc, getDoc } = useFireStore();
     const { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, provider, signInWithPopup } = useFirebaseAuth();
     const [firebaseErrror, setFirebaseError] = useState<string>("");
     const [loading, setLoading] = useState(false);
     const [googleLoading, setGoogleLoading] = useState(false)
 
-    const navigate = useNavigate();
-    
-    const { register, handleSubmit, formState: { errors }} = useForm<FormProps>({
+    const { register, handleSubmit, formState: { errors } } = useForm<FormProps>({
         resolver: zodResolver(formSchema)
     });
 
-    const onSubmit : SubmitHandler<FormProps> = async (data) => {
+    const onSubmit: SubmitHandler<FormProps> = async (data) => {
         setFirebaseError("");
         setLoading(true)
         try {
-            const result = formType == "register" ? await createUserWithEmailAndPassword(auth, data.email, data.password) : await signInWithEmailAndPassword(auth, data.email, data.password); 
+            const result = formType == "register" ? await createUserWithEmailAndPassword(auth, data.email, data.password) : await signInWithEmailAndPassword(auth, data.email, data.password);
             if (formType === "register") {
                 const { uid } = result.user;
                 const newUser = {
@@ -34,8 +31,8 @@ export const useFormValidation = (formType : string) => {
                 const docRef = doc(db, "users", uid);
                 await setDoc(docRef, newUser);
             }
-            navigate("/");
-        } catch(error : any) {
+            window.location.href = "/";
+        } catch (error: any) {
             setFirebaseError(error?.message || "Invalid credentials")
         } finally {
             setLoading(false)
@@ -60,8 +57,8 @@ export const useFormValidation = (formType : string) => {
                 const docRef = doc(db, "users", uid);
                 await setDoc(docRef, newUser);
             }
-            navigate("/");
-        } catch(error : any) {
+            window.location.href = "/"
+        } catch (error: any) {
             if (error?.message === "Firebase: Error (auth/cancelled-popup-request)." || error?.message === "Firebase: Error (auth/popup-closed-by-user).") return setFirebaseError("")
             setFirebaseError(error?.message)
         } finally {
