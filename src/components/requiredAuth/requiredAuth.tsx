cd2214542390c3a280642b0f3483b2c03ce144d8
jerome <jerome.studypurpose@gmail.com>
2024-07-20 21:01:01 +0800

    modify the structure of the routes

--- src/components/requiredAuth/requiredAuth.tsx
+++ src/components/requiredAuth/requiredAuth.tsx
@@ -1,16 +1,25 @@
 import { ReactNode } from "react";
 import { useAuth } from "../../context/authProvider";
 import { Navigate } from "react-router-dom";
+import { AuthLayout } from "../Layout/AuthLayout";
+import { useLocation } from "react-router-dom";
 
 export const RequiredAuth = ({ children }: { children: ReactNode }) => {
-    const { currentUser, loading : AuthLoading, isUsernameExist } = useAuth();
+    const { currentUser, loading: AuthLoading, isUsernameExist } = useAuth();
+    const location = useLocation();
+    const isSetUpRoute = location.pathname === "/setup-username"
+
     if (AuthLoading) return <h1>Loading</h1>
     if (!currentUser) return <Navigate to={'/login'} />
-    if (!isUsernameExist) return <Navigate to={'/setup-username'} />
+    if (!isUsernameExist && !isSetUpRoute) return <Navigate to={"/setup-username"} />
+    if (!isUsernameExist && isSetUpRoute) return children
+    if (isUsernameExist && isSetUpRoute) return <Navigate to={"/404"} />
 
     return (
         <>
-            {children}
+            <AuthLayout>
+                {children}
+            </AuthLayout>
         </>
     )
 
