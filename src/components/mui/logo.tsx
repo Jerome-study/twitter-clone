797a5068aab2e3224763cb17d67280553189454b
jerome <jerome.studypurpose@gmail.com>
2024-07-20 21:00:07 +0800

    make a conditional sizing of font

--- src/components/mui/logo.tsx
+++ src/components/mui/logo.tsx
@@ -1,8 +1,8 @@
 import Typography from '@mui/material/Typography';
 
-export const Logo = ({ inTopBar } : any) => {
+export const Logo = ({ inTopBar, size } : any) => {
   return (
-    <Typography pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', color: inTopBar ? '#fff' : '#1DA1F2' }}>
+    <Typography fontSize={size ? size : null} pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', color: inTopBar ? '#fff' : '#1DA1F2' }}>
       SocialTweet
     </Typography>
   );
