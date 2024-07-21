2a485c62a09d8791b7e5c53e9227d53da5a1f171
jerome <jerome.studypurpose@gmail.com>
2024-07-21 22:30:08 +0800

    add tweet props and user info props

--- src/models/typescript.ts
+++ src/models/typescript.ts
@@ -11,3 +11,20 @@ export interface InputProps {
     placeholder: string,
     type: string
 }
+
+export interface UserInfoProps {
+    username: string,
+    first_name : string,
+    last_name : string
+    email : string,
+    id : string
+}
+
+export interface TweetProps {
+    content : string,
+    createdAt: Date,
+    image : [string],
+    updatedAt : Date,
+    user_id : string,
+    id : string
+}
