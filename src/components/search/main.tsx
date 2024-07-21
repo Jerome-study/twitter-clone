ad0d7c22740f452b79f915fd506da7fe9045988c
jerome <jerome.studypurpose@gmail.com>
2024-07-21 22:29:28 +0800

    add functionality of searching a user

--- src/components/search/main.tsx
+++ src/components/search/main.tsx
@@ -1,5 +1,11 @@
-import { Fragment, useState } from "react"
+import { Fragment, lazy, Suspense, useState } from "react"
 import { TextField, Typography, Paper, Box, Divider } from '@mui/material';
+import { UserCardSkeleton } from "../mui/skeleton/UserCardSkeleton";
+import { CircularProgress } from '@mui/material';
+import { UserInfoProps } from "../../models/typescript";
+import { useSearch } from "../../hooks/useSearch";
+
+const UserCardLazy = lazy(() => import("./UserCard"))
 
 const trendingData = [
     { topic: "#ReactJS", tweets: "120K Tweets" },
@@ -16,7 +22,8 @@ const trendingData = [
 
 export const MainSearchComponent = () => {
     const [searchValue, setSearchValue] = useState('');
-
+    const { searchResult, loading } = useSearch(searchValue);
+    
     const handleChangeValue = (e: any) => {
         setSearchValue(e.target.value)
     }
@@ -26,11 +33,11 @@ export const MainSearchComponent = () => {
             <TextField
                 variant="outlined"
                 size="small"
-                placeholder="Search Twitter"
+                placeholder="Search SocialTweet User"
                 value={searchValue}
                 onChange={handleChangeValue}
                 sx={{
-                    p : 2,
+                    p: 2,
                     width: '100%',
                     '& .MuiOutlinedInput-root': {
                         borderRadius: 20
@@ -38,20 +45,46 @@ export const MainSearchComponent = () => {
                 }}
             />
 
-            <Box sx={{ display: searchValue? "none" : "block", px : 2 }}>
-                <Typography variant="h5" mb={2} fontWeight={900}>Trending Now</Typography>
-                {trendingData.map((trend, index) => {
-                    return(
-                        <Fragment key={index}>
-                            <Divider />
-                            <Paper key={index} sx={{ borderRadius: 0, py : 2}} elevation={0}>
-                                <Typography variant="body1" fontWeight={700}>{trend.topic}</Typography>
-                                <Typography variant="subtitle2" color="text.secondary">{trend.tweets}</Typography>
-                            </Paper>
-                        </Fragment>
-                    )
-                })}
-            </Box>
+            {(loading && searchValue) &&
+                Array.from(new Array(10)).map((_, index) => (
+                    <UserCardSkeleton key={index} />
+                ))
+            }
+
+            {(searchResult.length === 0 && !searchValue) &&
+                <Box sx={{ px: 2 }}>
+                    <Typography variant="h5" mb={2} fontWeight={900}>Trending Now</Typography>
+                    {trendingData.map((trend, index) => {
+                        return (
+                            <Fragment key={index}>
+                                <Divider />
+                                <Paper key={index} sx={{ borderRadius: 0, py: 2 }} elevation={0}>
+                                    <Typography variant="body1" fontWeight={700}>{trend.topic}</Typography>
+                                    <Typography variant="subtitle2" color="text.secondary">{trend.tweets}</Typography>
+                                </Paper>
+                            </Fragment>
+                        )
+                    })}
+                </Box>
+            }
+
+            {(searchResult.length > 0 && !loading) &&
+                <Suspense fallback={<CircularProgress />}>
+                    <Box>
+                        {searchResult.map((user: UserInfoProps, index: number) => (
+                            <UserCardLazy user={user} key={index} />
+                        ))}
+                    </Box>
+                </Suspense>
+            }
+
+            {(searchResult.length === 0 && searchValue) &&
+                <Box sx={{ display: "flex", height: "70vh", alignItems: "center" }}>
+                    <Typography fontWeight={600} align="center" variant="subtitle2">
+                        No Results found, please check your input if it is type correctly
+                    </Typography>
+                </Box>
+            }
 
         </>
     )
