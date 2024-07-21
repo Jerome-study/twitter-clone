a77b60b18bd976cbd7afcc2c38e9d6e176e4cc93
jerome <jerome.studypurpose@gmail.com>
2024-07-21 22:30:23 +0800

    create custom theme for colors

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,9 +1,28 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { red, grey } from '@mui/material/colors';
 
+declare module "@mui/material/styles" {
+    interface PaletteOptions {
+        TweetsColor?: {
+            displayName: string,
+            username: string
+        },
+        HomeComponentColors: {
+            divider: string
+        }
+    }
+}
 
 export const theme = createTheme({
     palette: {
-        mode: "light"
+        mode: "light",
+        TweetsColor : {
+            displayName: red[500],
+            username: grey[700]
+        },
+        HomeComponentColors : {
+            divider: grey[300]
+        }
     },
     typography: {
         fontFamily: 'Segoe UI, Arial, sans-serif'
