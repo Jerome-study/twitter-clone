cdc61e6a1921ccda41f6d4920561768d62c3baa6
jerome <jerome.studypurpose@gmail.com>
2024-07-21 22:29:01 +0800

    add a lazy loading and skeleton

--- src/components/home/HomeComponent.tsx
+++ src/components/home/HomeComponent.tsx
@@ -1,23 +1,24 @@
-import { Tweets } from "../tweets/Tweets";
 import { TweetComposer } from "./TweetComposer";
-import { Box, Typography } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { useTweet } from "../../context/tweetProvider";
 import { useResponsive } from "../../hooks/useResponsive";
+import { TweetSkeleton } from "../mui/skeleton/TweetSkeleton";
+import { lazy, Suspense } from "react";
+import { TweetProps } from "../../models/typescript";
+
+const LazyTweets = lazy(() => import("../tweets/Tweets"));
 
 export const HomeComponent = () => {
   const { currentUserTweets, tweetsLoading } = useTweet();
   const { isMobile } = useResponsive();
-
-  if (tweetsLoading) return <Typography variant="h1" height={"100vh"}>Loading</Typography>
 
   return (
     <>
       <Typography
         hidden
         sx={{
-          p: 2, display: {
-            lg: 'block'
-          }
+          p: 2, 
+          display: { lg: 'block' }
         }}
         component="h1"
         variant="h5"
@@ -25,17 +26,30 @@ export const HomeComponent = () => {
       >
         Home
       </Typography>
+
       <Box sx={{ borderTop: '1px solid #ccc', borderBottom: '1px solid #ccc' }}>
         <TweetComposer />
-        { !isMobile && <Box py={1} bgcolor={"#ccc"}></Box>}
+        {!isMobile && (
+          <Box py={1} bgcolor={"HomeComponentColors.divider"} />
+        )}
       </Box>
 
-      {currentUserTweets?.map((currentUserTweet: any) => {
-        return (
-          <Tweets currentUserTweet={currentUserTweet} key={currentUserTweet.id} />
-        )
-      })}
-
+      {tweetsLoading ? (
+        <Box>
+          {Array.from({ length: 10 }, (_, index) => (
+            <TweetSkeleton key={index} />
+          ))}
+        </Box>
+      ) : 
+        <Box>
+          <Suspense fallback={<CircularProgress />}>
+            {currentUserTweets?.map((tweet : TweetProps) => (
+              <LazyTweets currentUserTweet={tweet} key={tweet.id} />
+            ))}
+          </Suspense>
+        </Box>
+      }
+      
     </>
   );
-}
+};
