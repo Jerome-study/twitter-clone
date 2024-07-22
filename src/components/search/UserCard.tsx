0f8ef12091ec5459a08515151e529d5956db11cb
jerome <jerome.studypurpose@gmail.com>
2024-07-22 22:06:54 +0800

    add follow feature in button

--- src/components/search/UserCard.tsx
+++ src/components/search/UserCard.tsx
@@ -1,17 +1,26 @@
-import { Card, CardHeader, Skeleton, Typography } from "@mui/material"
+import { Card, CardHeader, Skeleton, Typography, Button } from "@mui/material"
 import { UserInfoProps } from "../../models/typescript"
+import { useAuth } from "../../context/authProvider"
+import { useFollowUser } from "../../hooks/useFollowUser";
 
-export const UserCard = ({ user } : { user : UserInfoProps }) => {
+export const UserCard = ({ user }: { user: UserInfoProps }) => {
+    const { currentUser } = useAuth();
+    const {isFollowing, handleFollowClick} = useFollowUser(currentUser.uid, user.id);
 
-    return(
+    return (
         <>
             <Card sx={{ borderRadius: 0 }}>
                 <CardHeader
-                        avatar={
-                            <Skeleton variant="circular" width={40} height={40} />
-                        }
-                        title={<Typography>{user.first_name + " " + user.last_name}</Typography>}
-                        subheader={<Typography color="TweetsColor.username">@{user.username}</Typography>}
+                    avatar={
+                        <Skeleton variant="circular" width={40} height={40} />
+                    }
+                    title={<Typography>{user.first_name + " " + user.last_name}</Typography>}
+                    subheader={<Typography color="TweetsColor.username">@{user.username}</Typography>}
+                    action={
+                        <Button variant="contained" color="primary" onClick={handleFollowClick}>
+                            {isFollowing ? "UNFOLLOW" : "FOLLOW"}
+                        </Button>
+                    }
                 />
             </Card>
         </>
