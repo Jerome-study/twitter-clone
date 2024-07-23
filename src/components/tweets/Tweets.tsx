efc1c7843678ec2bd693575bc6597e1ba15a5669
jerome <jerome.studypurpose@gmail.com>
2024-07-23 17:07:12 +0800

    add a time when thee tweets is created

--- src/components/tweets/Tweets.tsx
+++ src/components/tweets/Tweets.tsx
@@ -1,27 +1,29 @@
-import { Card, Box, Avatar, Typography, CardContent} from '@mui/material';
-import { useTweet } from '../../context/tweetProvider';
+import { Card, Box, Avatar, Typography, CardContent } from '@mui/material';
 import { TweetImages } from './TweetImages';
 import { useResponsive } from '../../hooks/useResponsive';
 import { ActionComponent } from './ActionComponent';
+import { useUpdateTime } from '../../hooks/useUpdateTime';
 
-export const Tweets = ({ currentUserTweet }: any) => {
+export const Tweets = ({ currentUserTweet, userInfo }: any) => {
     const avatarUrl = 'https://via.placeholder.com/150';
     const { isMobile } = useResponsive();
-    const { userInfo } = useTweet()
-
     const { first_name, last_name, username } = userInfo;
+    const { timeDisplay } = useUpdateTime(currentUserTweet.createdAt.toDate())
     
     return (
         <>
-            <Card sx={{bgcolor: "", borderRadius: 0, boxShadow: 0, borderBottom: '1px solid #ccc', display: 'flex', gap: 2, px: 2, py: 1.5 }}>
+            <Card sx={{ bgcolor: "", borderRadius: 0, boxShadow: 0, borderBottom: '1px solid #ccc', display: 'flex', gap: 2, px: 2, py: 1.5 }}>
                 <Box>
                     <Avatar alt="Remy Sharp" src={avatarUrl} />
                 </Box>
                 <Box sx={{ flex: 1 }}>
-                    <Typography variant={isMobile ? "subtitle1" : "h6"} fontWeight={900} >{first_name + " " + last_name}</Typography>
-                    <Typography fontSize={isMobile ? 13 : ""} variant='h1' color='TweetsColor.username' fontWeight={600} >@{username}</Typography>
+                    <Typography variant={isMobile ? "subtitle1" : "body1"} fontWeight={900} >
+                        {first_name + " " + last_name}
+                        <Typography fontSize={isMobile ? 14 : ""} component="span" variant='h1' color='TweetsColor.username' fontWeight={600} >@{username}</Typography>
+                        <Typography component="span" ml={2.5} variant='subtitle2' color='TweetsColor.username' fontWeight={600} >{timeDisplay}</Typography>
+                    </Typography>
                     <CardContent sx={{ px: 0, py: 1 }}>
-                        <Typography variant={isMobile ? "subtitle2" : "subtitle1"} fontWeight={600} mb={2}>
+                        <Typography variant={"subtitle2"} fontWeight={600} mb={2}>
                             {currentUserTweet.content}
                         </Typography>
                         {currentUserTweet.image?.length > 0 && <TweetImages images={currentUserTweet.image} />}
