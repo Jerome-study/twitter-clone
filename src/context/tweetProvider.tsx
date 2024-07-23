babe3dcc2752d5b398966f00d5cc6affbc84229c
jerome <jerome.studypurpose@gmail.com>
2024-07-23 17:07:39 +0800

    adds the followed users tweets using the custom hooks

--- src/context/tweetProvider.tsx
+++ src/context/tweetProvider.tsx
@@ -2,6 +2,8 @@ import { createContext, useContext, ReactNode } from "react";
 import { useGetTweets } from "../hooks/useGetTweets"
 import { useAuth } from "./authProvider"
 import { useGetTweetsUserInfo } from "../hooks/useGetUserInfo";
+import { useGetFollowing } from "../hooks/useGetFollowing";
+import { useGetFollowingTweets } from "../hooks/useGetFollowingTweets";
 
 
 const TweetContext = createContext<any>({});
@@ -12,11 +14,13 @@ export const useTweet = () => {
 
 export const TweetProvider = ({ children } : { children : ReactNode }) => {
     const { currentUser } = useAuth(); 
-    const { userInfo, loading: userInfoLoading, error: userInfoError } = useGetTweetsUserInfo(currentUser.uid)
+    const { userInfo, loading: userInfoLoading } = useGetTweetsUserInfo(currentUser.uid)
+    const { currentUserFollowing, loading : currentUserFollowingLoading } = useGetFollowing(currentUser.uid);
+    const { currentUserFollowingTweets, loading : currentUserFollowingTweetsLoading } = useGetFollowingTweets(currentUserFollowing);
     const { currentUserTweets, loading : tweetsLoading } = useGetTweets(currentUser.uid);
-    
+    const tweetProviderLoading = tweetsLoading || currentUserFollowingLoading || currentUserFollowingTweetsLoading || userInfoLoading
     return(
-        <TweetContext.Provider value={{ currentUserTweets, userInfo, tweetsLoading, userInfoLoading, userInfoError }}>
+        <TweetContext.Provider value={{ currentUserTweets, tweetProviderLoading, currentUserFollowingTweets, userInfo }}>
             { children }
         </TweetContext.Provider>
     )
