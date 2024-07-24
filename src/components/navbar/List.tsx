9ae053e1d4a17a40852e6425c293e686217c58fb
jerome <jerome.studypurpose@gmail.com>
2024-07-24 20:06:17 +0800

    add other links and modify designs, add a function to tweet button to focus the tweet composer

--- src/components/navbar/List.tsx
+++ src/components/navbar/List.tsx
@@ -5,56 +5,72 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import SettingsIcon from '@mui/icons-material/Settings';
 import Collapse from '@mui/material/Collapse';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { Typography, Button } from '@mui/material';
 import { Logo } from '../mui/logo';
 import { NavList } from './const';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../../hooks/useLogout';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
+import { useResponsive } from '../../hooks/useResponsive';
+import { useTweet } from '../../context/tweetProvider';
 
 export const NavigationLeftList = () => {
     const [open, setOpen] = useState(false);
     const { logout } = useLogout();
+    const { isMobile } = useResponsive();
+    const { focusInput } = useTweet()
+
     const handleClick = () => {
         setOpen(!open);
     };
+
     return (
         <>
-                <List sx={{ px: { lg: 5 }}}>
-                    <ListItem >
-                        <Logo />
-                    </ListItem>
-                    
-                    {NavList.map((text) => (
-                        <ListItem sx={{ bgcolor: "transparent"}} key={text.name} disablePadding>
-                            <ListItemButton component={Link} to={text.path}>
+            <List sx={{ px: { xs: 2, lg: 5 } }}>
+                <ListItem >
+                    <Logo />
+                </ListItem>
+
+                {NavList.map((text) => (
+                    ((text.isLargeView && !isMobile ) || text.isMobileView && isMobile) &&
+                    <Fragment key={text.name}>
+                        <ListItem sx={{ bgcolor: "transparent", mb: 1 }} disablePadding>
+                            <ListItemButton component={Link} to={text.path || "#"} onClick={() => text?.isCollapse ? handleClick() : undefined}>
                                 <ListItemIcon>
                                     {text.icon}
                                 </ListItemIcon>
-                                <ListItemText primary={text.name} />
+                                <ListItemText
+                                    primary={
+                                        <Typography variant='h6' sx={{ fontSize: { xs: 17, lg: 20 }, fontWeight: 700 }}>
+                                            {text.name}
+                                        </Typography>
+                                    }
+                                />
+                                {text.isCollapse && <>{open ? <ExpandLess /> : <ExpandMore />}</>}
                             </ListItemButton>
                         </ListItem>
-                    ))}
-                    <ListItemButton onClick={handleClick}>
-                        <ListItemIcon>
-                            <SettingsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Settings" />
-                        {open ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <List component="div" disablePadding>
-                            <ListItemButton onClick={logout} sx={{ pl: 4 }}>
-                                <ListItemIcon>
-                                    <LogoutIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Logout" />
-                            </ListItemButton>
-                        </List>
-                    </Collapse>
-                </List>
+                        {text.isCollapse &&
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <List component="div" disablePadding>
+                                    <ListItemButton onClick={logout} sx={{ pl: 4 }}>
+                                        <ListItemIcon>
+                                            <LogoutIcon sx={{ fontSize: { xs: 12, lg: 15}, color: 'info.light' }} />
+                                        </ListItemIcon>
+                                        <ListItemText primary={
+                                            <Typography sx={{ fontSize: { xs: 13, lg: 15 }, fontWeight: 600 }}>
+                                                Logout
+                                            </Typography>}
+                                        />
+                                    </ListItemButton>
+                                </List>
+                            </Collapse>
+                        }
+                    </Fragment> 
+                ))}
+                {!isMobile && <Button onClick={() => focusInput()} variant='contained' sx={{ borderRadius: 50, width: '100%', py:2,  mt : 2, fontWeight: 700, fontSize: { xs: 13, lg: 17} }}>Tweet</Button>}
+            </List>
         </>
     )
 }
--- src/components/navbar/const.tsx
+++ src/components/navbar/const.tsx
@@ -2,36 +2,67 @@ import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MailIcon from '@mui/icons-material/Mail';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 export const NavList = [
-    {
-        name: "Home",
-        path: "/",
-        icon: <HomeIcon />
-    },
-    {
-        name: "Profile",
-        path: "/profile",
-        icon: <AccountCircleIcon />
-    }
+  {
+    name: "Home",
+    path: "/",
+    icon: <HomeOutlinedIcon sx={{ fontSize: { xs: 20, lg: 25 }, color: 'info.light' }} />,
+    isMobileView: true,
+    isLargeView : true
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: <Person2OutlinedIcon sx={{ fontSize: { xs: 20, lg: 25 }, color: 'info.light' }} />,
+    isMobileView: true,
+    isLargeView : true
+  },
+  {
+    name: "Notifications",
+    icon: <NotificationsNoneOutlinedIcon sx={{ fontSize: { xs: 20, lg: 25 }, color: 'info.light' }} />,
+    isLargeView : true,
+  },
+  {
+    name: "Messages",
+    icon: <EmailOutlinedIcon sx={{ fontSize: { xs: 20, lg: 25 }, color: 'info.light' }}  />,
+    isLargeView : true,
+  },
+  {
+    name: "Bookmarks",
+    icon: <BookmarkBorderOutlinedIcon sx={{ fontSize: { xs: 20, lg: 25 }, color: 'info.light'}} />,
+    isMobileView: true,
+    isLargeView : true,
+  },
+  {
+    name: "Settings",
+    icon: <SettingsOutlinedIcon sx={{ fontSize: { xs: 20, lg: 25 }, color: 'info.light'}} />,
+    isMobileView: true,
+    isLargeView : true,
+    isCollapse: true
+  },
 ]
 
 export const BottomNavlist = [
-    {
-      name: "Home",
-      icon: <HomeIcon />
-    },
-    {
-      name: "Explore",
-      icon: <SearchIcon />
-    },
-    {
-      name: "Notifications",
-      icon: <NotificationsIcon />
-    },
-    {
-      name: "Messages",
-      icon: <MailIcon />
-    }
-  ];
+  {
+    name: "Home",
+    icon: <HomeIcon />
+  },
+  {
+    name: "Explore",
+    icon: <SearchIcon />
+  },
+  {
+    name: "Notifications",
+    icon: <NotificationsIcon />
+  },
+  {
+    name: "Messages",
+    icon: <MailIcon />
+  }
+];
