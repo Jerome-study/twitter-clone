883152d71d633a195117cf908a3d86375ade6036
jerome <jerome.studypurpose@gmail.com>
2024-08-10 18:36:58 +0800

    create an object style for sx props

---
+++ src/components/navbar/styles.ts
@@ -0,0 +1,46 @@
+export const styles = {
+    appBarStyle: { 
+        backgroundColor: 'custom.blue', 
+        py: 1 
+    },
+    emptBoxStyle: { 
+        flexGrow: 1 
+    },
+    listContainerStyle: { 
+        px: { xs: 2, lg: 5 } 
+    },
+    listItemContainerStyle: {
+        bgcolor: "transparent", 
+        mb: 1
+    },
+    listNameStyle: { 
+        fontSize: { xs: 17, lg: 20 }, 
+        fontWeight: 700 
+    },
+    listIconStyle: { 
+        fontSize: { xs: 20, lg: 25 }, 
+        color: 'custom.blue' 
+    },
+    collapseListIconStyle: { 
+        fontSize: { xs: 12, lg: 15}, 
+        color: 'custom.blue' 
+    },
+    collapseListNameStyle: {
+        fontSize: { xs: 13, lg: 15 }, 
+        fontWeight: 600
+    },
+    tweetButtonStyle: { 
+        borderRadius: 50, 
+        width: '100%',
+        py:2,  
+        mt : 2, 
+        fontWeight: 700, 
+        fontSize: { xs: 13, lg: 17} 
+    },
+    bottomNavStyle: { 
+        position: 'fixed', 
+        bottom: 0, 
+        left: 0, 
+        right: 0 
+    }
+}
