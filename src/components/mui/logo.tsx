1eda5940317c7d5a91b53fd09a96da8175bf9959
jerome <jerome.studypurpose@gmail.com>
2024-08-11 20:42:46 +0800

    make topbar logo blue when in mobile mode

--- src/components/mui/logo.tsx
+++ src/components/mui/logo.tsx
@@ -2,7 +2,7 @@ import Typography from '@mui/material/Typography';
 
 export const Logo = ({ inTopBar, size } : any) => {
   return (
-    <Typography fontSize={size ? size : null} pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', color: inTopBar ? '#fff' : '#1DA1F2' }}>
+    <Typography fontSize={size ? size : null} pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', color: inTopBar ? 'custom.blue' : '#1DA1F2' }}>
       SocialTweet
     </Typography>
   );
