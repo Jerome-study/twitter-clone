b5544f269574f1408e49eb445f17ba2cc2568a20
jerome <jerome.studypurpose@gmail.com>
2024-08-12 16:08:05 +0800

    change logo color

--- src/components/mui/logo.tsx
+++ src/components/mui/logo.tsx
@@ -1,8 +1,8 @@
 import Typography from '@mui/material/Typography';
 
-export const Logo = ({ inTopBar, size } : any) => {
+export const Logo = ({ inTopBar, size, isCenter } : any) => {
   return (
-    <Typography fontSize={size ? size : null} pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', color: inTopBar ? 'custom.blue' : '#1DA1F2' }}>
+    <Typography fontSize={size ? size : null} pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: isCenter === undefined? "center" : "left", fontFamily: 'Arial, sans-serif', fontWeight: '700', color: "custom.blue" }}>
       SocialTweet
     </Typography>
   );
