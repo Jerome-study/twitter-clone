bf37fd969fa026a17fda7434f3056315860f97a0
jerome <jerome.studypurpose@gmail.com>
2024-08-13 17:21:56 +0800

    create object styles for home page

---
+++ src/components/home/styles.ts
@@ -0,0 +1,8 @@
+export const styles = {
+    tweetContainerStyle: { 
+        display: "grid", 
+        gap: 2, mt: { lg: 0 }, 
+        maxWidth: { sm: "sm", lg: "xl" }, 
+        marginInline: "auto" 
+    }
+}
