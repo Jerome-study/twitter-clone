f1a533338c3f5f13a937f0731d44f0e6152e83e9
jerome <jerome.studypurpose@gmail.com>
2024-08-13 17:23:38 +0800

    modify the designs that matches the actual tweet

--- src/components/mui/skeleton/TweetSkeletonMultiple.tsx
+++ src/components/mui/skeleton/TweetSkeletonMultiple.tsx
@@ -3,7 +3,7 @@ import { TweetSkeleton } from "./TweetSkeleton"
 
 export const TweetSkeletonMultiple = () => {
     return (
-        <Box>
+        <Box sx={{ display: "grid", gap: 2, mt: { lg: 0 } }}>
             {Array.from({ length: 10 }, (_, index) => (
                 <TweetSkeleton key={index} />
             ))}
