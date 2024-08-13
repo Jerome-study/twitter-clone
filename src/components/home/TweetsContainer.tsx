0b2ff9e9b520a7cbcdc0834d03057eaf2c464c57
jerome <jerome.studypurpose@gmail.com>
2024-08-13 17:23:21 +0800

    container modify when in md  view

--- src/components/home/TweetsContainer.tsx
+++ src/components/home/TweetsContainer.tsx
@@ -2,6 +2,8 @@ import { Box } from "@mui/material";
 import { lazy, Suspense } from "react";
 import { TweetSkeletonMultiple } from "../mui/skeleton/TweetSkeletonMultiple";
 import { useTweet } from "../../context/tweetProvider";
+import { styles } from "./styles";
+
 const LazyTweets = lazy(() => import("../Tweets/Tweets"));
 export const TweetsContainer = () => {
     const { currentUserTweets, currentUserFollowingTweets, tweetProviderLoading, userInfo } = useTweet();
@@ -12,7 +14,7 @@ export const TweetsContainer = () => {
                 <TweetSkeletonMultiple />
             ) :
                 <>
-                    <Box sx={{ display: "grid", gap: 2, mt: { lg: 0 } }}>
+                    <Box sx={styles.tweetContainerStyle}>
                         <Suspense fallback={<TweetSkeletonMultiple />}>
                             {[...currentUserTweets, ...currentUserFollowingTweets].sort((a, b) => b.createdAt - a.createdAt)?.map((tweet: any) => (
                                 <LazyTweets currentUserTweet={tweet} userInfo={tweet.userInfo || userInfo} key={tweet.id} />
