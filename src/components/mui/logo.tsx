74f2bd69390ebe6b343ff3e288612bc471235ae4
jerome <jerome.studypurpose@gmail.com>
2024-08-13 14:11:00 +0800

    add a useLocation to determine the color of the logo

--- src/components/mui/logo.tsx
+++ src/components/mui/logo.tsx
@@ -1,8 +1,11 @@
 import Typography from '@mui/material/Typography';
+import { useLocation } from 'react-router-dom';
 
 export const Logo = ({ inTopBar, size, isCenter } : any) => {
+  const location = useLocation();
+  const condition = location.pathname === "/register" || location.pathname === "/login" ? true : false
   return (
-    <Typography fontSize={size ? size : null} pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: isCenter === undefined? "center" : "left", fontFamily: 'Arial, sans-serif', fontWeight: '700', color: "custom.blue" }}>
+    <Typography fontSize={size ? size : null} pr={inTopBar ? 2 : 0} variant={inTopBar ? "h6" : "h4"} component="h1" sx={{ textAlign: isCenter === undefined? "center" : "left", fontFamily: 'Arial, sans-serif', fontWeight: '700', color: condition? "custom.white" :  "custom.blue" }}>
       SocialTweet
     </Typography>
   );
