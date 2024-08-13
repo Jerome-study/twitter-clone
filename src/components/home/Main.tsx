becc2eaa2793cf7b5576ad51732ea9dc60c0d111
jerome <jerome.studypurpose@gmail.com>
2024-08-13 14:10:40 +0800

    simplify the home component

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -1,25 +1,20 @@
-import { TweetComposer } from "./TweetComposer";
-import { Box, CircularProgress, Typography } from "@mui/material";
-import { useTweet } from "../../context/tweetProvider";
+import { TweetBox } from "./TweetComposer/TweetBox";
+import { Box, Typography } from "@mui/material";
 import { useResponsive } from "../../hooks/useResponsive";
-import { TweetSkeleton } from "../mui/skeleton/TweetSkeleton";
-import { lazy, Suspense } from "react";
-
-const LazyTweets = lazy(() => import("../Tweets/Tweets"));
+import { TweetsContainer } from "./TweetsContainer";
 
 export const MainHomeComponent = () => {
-  const { currentUserTweets, currentUserFollowingTweets, tweetProviderLoading } = useTweet();
   const { isMobile } = useResponsive();
-  const { userInfo } = useTweet();
 
   return (
     <>
-      { !isMobile && <Typography sx={{
-          p: 2,
-          bgcolor: "custom.white",
-          mb: 2,
-          borderRadius: 5
-        }}
+      {/* Header */}
+      {!isMobile && <Typography sx={{
+        p: 2,
+        bgcolor: "custom.white",
+        mb: 2,
+        borderRadius: 5
+      }}
         component="h1"
         variant="h5"
         fontWeight={800}
@@ -27,27 +22,13 @@ export const MainHomeComponent = () => {
         Home
       </Typography>}
 
-      <Box sx={{ bgcolor: "custom.white", mb : 2, borderRadius: 5 }}>
-        <TweetComposer />
+      {/* Tweet Box */}
+      <Box sx={{ bgcolor: "custom.white", mb: 2, borderRadius: 5 }}>
+        <TweetBox />
       </Box>
 
-      {(tweetProviderLoading) ? (
-        <Box>
-          {Array.from({ length: 10 }, (_, index) => (
-            <TweetSkeleton key={index} />
-          ))}
-        </Box>
-      ) :
-        <>
-          <Box sx={{ display: "grid", gap: 2, mt: { lg: 0}}}>
-            <Suspense fallback={<CircularProgress />}>
-              {[...currentUserTweets, ...currentUserFollowingTweets].sort((a,b) => b.createdAt - a.createdAt )?.map((tweet: any) => (
-                <LazyTweets currentUserTweet={tweet} userInfo={tweet.userInfo || userInfo} key={tweet.id} />
-              ))}
-            </Suspense>
-          </Box>
-        </>
-      }
+      {/* Tweets */}
+      <TweetsContainer />
     </>
   );
 };
