9256009d0b246f3fa3056b772bee9f3f38e25887
jerome <jerome.studypurpose@gmail.com>
2024-08-14 22:35:10 +0800

    optimize the change of components of children and search component in mobile view

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -6,15 +6,25 @@ import { TopAndLeftSidebar } from "./TopAndLeftSide";
 import { BottomNavbar } from "../Navbar/BottomNav";
 import { useResponsive } from "../../hooks/useResponsive";
 import { TweetProvider } from "../../context/tweetProvider";
-import { MainSearchComponent } from "../Search/main";
 import { styles } from "./styles";
 
 export const AuthLayout = ({ children }: { children: ReactNode }) => {
     const { isMobile } = useResponsive();
     const [currentAction, setCurrentAction] = useState("Home");
-    const condition = currentAction === "Home" ? children : currentAction === "Explore" ? <MainSearchComponent /> : <h1>Coming Soon</h1>
+    const [currentPosition, setCUrrentPosition] = useState<any>(null);
     const handleBottomNavAction = (action: string) => {
         setCurrentAction(action)
+    }
+
+    const homeCurrentPosition = () => {
+        if (currentAction === "Home") {
+            setCUrrentPosition(window.pageYOffset)
+            window.scrollTo(0, 0)
+        } else {
+           setTimeout(() => {
+            window.scrollTo(0, currentPosition)
+           }, 100)
+        }
     }
 
     useEffect(() => {
@@ -29,15 +39,17 @@ export const AuthLayout = ({ children }: { children: ReactNode }) => {
                         <Grid item lg={3} xs={12}>
                             <TopAndLeftSidebar />
                         </Grid>
-                        <Grid item lg={5} xs={12} >
-                            <MainContent>{condition}</MainContent>
+                        <Grid item lg={5} xs={12}>
+                            <MainContent currentAction={currentAction}>
+                                {children}
+                            </MainContent>
                         </Grid>
                         {!isMobile ?
                             <Grid item lg={3} xs={12}>
                                 <RightSidebar />
                             </Grid>
                             :
-                            <BottomNavbar handleBottomNavAction={handleBottomNavAction} />
+                            <BottomNavbar homeCurrentPosition={homeCurrentPosition} handleBottomNavAction={handleBottomNavAction} />
                         }
                     </Grid>
                 </Box>
--- src/components/Layout/MainContent.tsx
+++ src/components/Layout/MainContent.tsx
@@ -1,9 +1,21 @@
 import { ReactNode } from "react";
 import { Box } from "@mui/material";
 import { styles } from "./styles";
+import { MainSearchComponent } from "../Search/main";
+import { useResponsive } from "../../hooks/useResponsive";
 
-export const MainContent = ({ children }: { children: ReactNode }) => (
-    <Box sx={styles.mainBoxStyle} >
-        {children}
-    </Box>
-);
+export const MainContent = ({ children, currentAction }: { children: ReactNode, currentAction: string }) => {
+    const isHome = true
+    const { isMobile } = useResponsive();
+    return (
+        <Box sx={styles.mainBoxStyle(currentAction)}>
+            <Box sx={styles.childrenStyle(currentAction, isHome)}>
+                {children}
+            </Box>
+            { isMobile && 
+            <Box sx={styles.childrenStyle(currentAction)}>
+                <MainSearchComponent />
+            </Box>}
+        </Box>
+    )
+};
